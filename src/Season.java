8021e364a0e5c018c0fdc19968a32ecefb7d327f
Volt <sonianadashkevich@gmail.com>
2019-11-26 12:31:17 +0100

    metoda przeniesiona do klasy enuma, bledy obsluzone

--- src/Season.java
+++ src/Season.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public enum Season {
     WINTER("Zima", new String[]{"Grudzien", "Styczen", "Luty"}),
     SPRING("Wiosna", new String[]{"Marzec", "Kwiecien", "Maj"}),
@@ -18,6 +20,22 @@ public enum Season {
     }
 
     public String[] getMonths() {
+        return months;
+    }
+
+    public static String[] getMonthsArray() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Wprowadz nazwe pory roku");
+        String line = input.nextLine();
+        String[] months = null;
+        for (Season s : Season.values()) {
+            if (line.equals(s.getName())) {
+                months = s.getMonths();
+            } else {
+                throw new IllegalArgumentException("Wprowadzono bledna nazwe pory roku");
+            }
+        }
+        input.close();
         return months;
     }
 }
--- src/Test2.java
+++ src/Test2.java
@@ -1,22 +1,12 @@
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(getMonths()));
-    }
-
-    private static String[] getMonths() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Wprowadz nazwe pory roku");
-        String line = input.nextLine();
-        String[] months = null;
-        for (Season s : Season.values()) {
-            if (line.equals(s.getName())) {
-                months = s.getMonths();
-            }
+        try {
+            System.out.println(Arrays.toString(Season.getMonthsArray()));
+        } catch (InputMismatchException | IllegalArgumentException e) {
+            System.err.println(e.getMessage());
         }
-        input.close();
-        return months;
     }
 }
